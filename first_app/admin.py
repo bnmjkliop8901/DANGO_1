08b82ad6d2494af925e8ce5ae34053a4b88addc2
Soheil <greenroomblackroom@gmail.com>
2025-05-09 21:11:24 +0330

    third_attmept

--- first_app/admin.py
+++ first_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from first_app.models import BlogPost , PostAuthor , Comment
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(PostAuthor)
+class PostAuthorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
--- first_app/models.py
+++ first_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class PostAuthor(models.Model):
+    first_name = models.CharField(max_length=55)
+    last_name = models.CharField(max_length=55)
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=55 , unique=True )
+    content = models.TextField(null = True , blank=True)
+    is_shown = models.BooleanField(default = True)
+    created_at = models.DateTimeField()
+    authors = models.ManyToManyField(PostAuthor)
+
+class Comment(models.Model):
+    blog_post = models.ForeignKey(BlogPost , on_delete = models.CASCADE)
+    author = models.CharField(max_length=55)
+    text = models.TextField()
